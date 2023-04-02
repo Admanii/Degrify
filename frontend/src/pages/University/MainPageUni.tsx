3289b9cd2f17bf3111d1cd282531cbde929f4863
Ahmed Riaz <ahmedriaz-50@hotmail.com>
2023-04-03 03:58:38 +0500

    Admani is here

---
+++ frontend/src/pages/University/MainPageUni.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../../components/general/Layout'
+import HeyUni from './Components/HeyUni'
+
+const MainPageUni = () => {
+  return (
+ 
+    <Layout>
+      <HeyUni Text='Hey IBA -'/>
+  
+    </Layout>
+ 
+  )
+}
+
+export default MainPageUni
