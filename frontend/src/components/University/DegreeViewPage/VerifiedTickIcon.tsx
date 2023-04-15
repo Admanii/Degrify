37e0bd8c0032e37859620cfe4c53ca8662ea6eb0
Ahmed Riaz <ahmedriaz-50@hotmail.com>
2023-04-16 03:50:47 +0500

    Paragraph dynamic

--- frontend/src/components/University/DegreeViewPage/VerifiedTickIcon.tsx
+++ frontend/src/components/University/DegreeViewPage/VerifiedTickIcon.tsx
@@ -7,15 +7,20 @@ interface Props {
 
 var verified = false;
 
+
 function VerifiedTickIcon({ size }: Props) {
+  var paraText = verified === true ? "This Degree Has been verified by HEC" : "This degrees is pending approval from the Higher Education Commission";
+
   return (
     <div className='flex flex-row justify-start py-4'>
       {/* <img src={IMAGES.unverified_icon}></img> */}
+
       <img src={
         // ADD RELEVANT LOGOS
         verified === true ? IMAGES.home_active_icon : IMAGES.home_icon
       }></img>
-      <p>This Degree Has been verified by HEC</p>
+      
+      <p className='px-2'>{paraText}</p>
     </div>
   );
 }
