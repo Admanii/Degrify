9c2c71cdd25d8801250ad94c99ff887b3ae30335
AhmedEdhii <ahmededhi49@gmail.com>
2023-04-17 06:25:09 +0500

    access token from localhost fixed

--- frontend/src/store/service/degreeServices.tsx
+++ frontend/src/store/service/degreeServices.tsx
@@ -1,25 +1,43 @@
 import api from "../api";
 
 var accessToken = '';
+var config = {}
 
-if (localStorage.getItem('accessToken')) {
-    accessToken = JSON.parse(localStorage.getItem('accessToken') ?? '');
+function getAccessToken(): Promise<string> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const accessToken = localStorage.getItem('accessToken');
+            resolve(accessToken ?? '');
+        }, 0);
+    });
 }
 
-const config = {
-    headers: {
-        'auth': accessToken,
+async function getConfig() {
+    const token = await getAccessToken();
+    accessToken = token.substring(1, token.length - 1)
+    config = {
+        headers: {
+            'auth': accessToken,
+        }
+        // headers: {
+        //     'Content-Type': 'application/json',
+        // },
     }
-    // headers: {
-    //     'Content-Type': 'application/json',
-    // },
 }
 
-export const getAllDegrees = () => {
+
+export const getAllDegrees = async () => {
+    await getConfig();
+    console.log("accessToken:    " + accessToken)
     return api.get("/getalldegreeshec", config);
+    // return api.get("/getalldegreeshec", {
+    //     headers: {
+    //         'auth': accessToken,
+    //     }
+    // });
 };
 
-export const getAllDegreesbyUniId = (organisation_id: string) => {
+export const getAllDegreesbyUniId = async (organisation_id: string) => {
+    await getConfig();
     return api.get(`getalldegreesuniversity/${organisation_id}`, config);
-  };
-  
+};
