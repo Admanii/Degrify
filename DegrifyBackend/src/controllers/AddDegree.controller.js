3808aabe38806a9e84e4ef8255a800f9222e9508
AhmedEdhii <ahmededhi49@gmail.com>
2023-05-01 03:27:00 +0500

    try catch fix

--- DegrifyBackend/src/controllers/AddDegree.controller.js
+++ DegrifyBackend/src/controllers/AddDegree.controller.js
@@ -4,21 +4,21 @@ import { statusCode } from "../utils/constant.js";
 import { jsonGenerate } from "../utils/helper.js";
 
 export const AddDegree = async (req, res) => {
-  const degreeExist = await Degree.findOne({
-    $or: [
-      {
-        studentID: req.query.student_id,
-      },
-    ],
-  });
-
-  if (degreeExist) {
-    return res.json(
-      jsonGenerate(statusCode.UNPROCESSABLE_ENTITY, "Degree already Exists")
-    );
-  }
-
   try {
+    const degreeExist = await Degree.findOne({
+      $or: [
+        {
+          studentID: req.query.student_id,
+        },
+      ],
+    });
+
+    if (degreeExist) {
+      return res.json(
+        jsonGenerate(statusCode.UNPROCESSABLE_ENTITY, "Degree already Exists")
+      );
+    }
+
     const result = await Degree.create({
       studentID: req.query.student_id,
       organisationID: req.query.organisation_id,
