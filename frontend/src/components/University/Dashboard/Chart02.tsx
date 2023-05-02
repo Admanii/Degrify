4a1cbea106498fd77bd6fb2d513647feac1a2316
Ahmed Riaz <ahmedriaz-50@hotmail.com>
2023-05-03 01:16:48 +0500

    Chart02

--- frontend/src/components/University/Dashboard/Chart02.tsx
+++ frontend/src/components/University/Dashboard/Chart02.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useRef } from 'react';
 import ApexCharts from "apexcharts";
 import { useSelector } from 'react-redux';
 import { DegreesByProgram } from '../../../store/slice/degreeSlice';
@@ -9,21 +9,123 @@ interface Props {
   id: string;
 }
 
-class Chart02 extends Component<Props> {
-  // degreesByProgram = useSelector(DegreesByProgram);
-  private chartRef: any;
+// class Chart02 extends Component<Props> {
+//   // degreesByProgram = useSelector(DegreesByProgram);
+  
+//   private chartRef: any;
 
-  componentDidMount() {
+//   componentDidMount() {
+//     const chartTwoOptions = {
+//       series: [
+//         {
+//           name: "Students",
+//           data: [44, 55, 41, 67, 22, 43],
+//         },
+//         // {
+//         //   name: "Revenue",
+//         //   data: [13, 23, 20, 8, 13, 27, 15],
+//         // },
+//       ],
+//       colors: ["#3056D3", "#80CAEE"],
+//       chart: {
+//         type: "bar",
+//         height: 335,
+//         stacked: true,
+//         toolbar: {
+//           show: false,
+//         },
+//         zoom: {
+//           enabled: false,
+//         },
+//       },
+
+//       responsive: [
+//         {
+//           breakpoint: 1536,
+//           options: {
+//             plotOptions: {
+//               bar: {
+//                 borderRadius: 0,
+//                 columnWidth: "25%",
+//               },
+//             },
+//           },
+//         },
+//       ],
+//       plotOptions: {
+//         bar: {
+//           horizontal: false,
+//           borderRadius: 0,
+//           columnWidth: "25%",
+//           borderRadiusApplication: "end",
+//           borderRadiusWhenStacked: "last",
+//         },
+//       },
+//       dataLabels: {
+//         enabled: false,
+//       },
+
+//       xaxis: {
+//         categories: ["BBA", "BSCS", "ACF", "BSSS", "BECO", "BSEM"],
+//       },
+//       legend: {
+//         position: "top",
+//         horizontalAlign: "left",
+//         fontFamily: "Satoshi",
+//         fontWeight: 500,
+//         fontSize: "14px",
+
+//         markers: {
+//           radius: 99,
+//         },
+//       },
+//       fill: {
+//         opacity: 1,
+//       },
+//     };
+
+//     const chartSelector = document.querySelectorAll(`#${this.props.id}`);
+
+//     if (chartSelector.length) {
+//       const chartTwo = new ApexCharts(
+//         document.querySelector(`#${this.props.id}`),
+//         chartTwoOptions
+//       );
+//       chartTwo.render();
+//       this.chartRef = chartTwo;
+//     }
+//   }
+
+//   componentWillUnmount() {
+//     this.chartRef && this.chartRef.destroy();
+//   }
+
+//   render() {
+//     return <div id={this.props.id}>
+//     </div>;
+//   }
+// }
+const Chart02: React.FC<Props> = ({ id }) => {
+  const chartRef = useRef<any>(null);
+  const degreesByProgram = useSelector(DegreesByProgram);
+  console.log("degreesByProgram: "+degreesByProgram[0]._id)
+
+  function searchYearCount(program: { toString: () => string; }) {
+    for (let i = 0; i < degreesByProgram.length; i++) {
+      if (degreesByProgram[i]._id === program.toString()) {
+        return degreesByProgram[i].count;
+      }
+    }
+    return 0;
+  }
+
+  useEffect(() => {
     const chartTwoOptions = {
       series: [
         {
           name: "Students",
           data: [44, 55, 41, 67, 22, 43],
         },
-        // {
-        //   name: "Revenue",
-        //   data: [13, 23, 20, 8, 13, 27, 15],
-        // },
       ],
       colors: ["#3056D3", "#80CAEE"],
       chart: {
@@ -83,26 +185,17 @@ class Chart02 extends Component<Props> {
       },
     };
 
-    const chartSelector = document.querySelectorAll(`#${this.props.id}`);
-
-    if (chartSelector.length) {
-      const chartTwo = new ApexCharts(
-        document.querySelector(`#${this.props.id}`),
-        chartTwoOptions
-      );
+    if (chartRef.current) {
+      const chartTwo = new ApexCharts(chartRef.current, chartTwoOptions);
       chartTwo.render();
-      this.chartRef = chartTwo;
+      return () => {
+        chartTwo.destroy();
+      };
     }
-  }
+  }, [chartRef, id]);
 
-  componentWillUnmount() {
-    this.chartRef && this.chartRef.destroy();
-  }
-
-  render() {
-    return <div id={this.props.id}></div>;
-  }
-}
+  return <div id={id} ref={chartRef} />;
+};
 
 export default Chart02;
 
