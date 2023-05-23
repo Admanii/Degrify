605923df6c8377086888b7e7b0cf5ea40e8afd0a
Ahmed Riaz <ahmedriaz-50@hotmail.com>
2023-05-23 08:31:04 +0500

    heyhec

--- frontend/src/components/HEC/Dashboard/HeyHec.tsx
+++ frontend/src/components/HEC/Dashboard/HeyHec.tsx
@@ -13,12 +13,7 @@ const HeyHec = () => {
   const { userInfo } = useSelector((state: any) => state.auth)
 
   return (
-    <div>
-      {/* <div className="flex flex-row ...">
-      <h1 className="font-bold text-2xl mt-6 ml-4">
-        {Text} <span className="font-normal">Here's what is happening today</span>
-      </h1>
-    </div> */}
+    <div className="pb-4">
       <HeadingWithSpan Text={userInfo?.user?.name + " -"} SpanText={"Here's what is happening today"} marginTop={"6"} />
       <AnalyticsSection />
       <div className="h-100 flex" >
